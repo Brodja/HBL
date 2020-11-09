37e682bc4f9d7c5956a05ade93a3541f5b734e18
Bohdan Derkach <body1906@gmail.com>
2020-11-09 21:23:51 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,73 @@
+function go() {
+  // $('#love').fadeTo(1000, 0.5);
+  $("#love").fadeOut(2000, 0);
+  setTimeout(func1, 2000);
+  setTimeout(func2, 4000);
+  setTimeout(func11, 4000);
+  setTimeout(func3, 6000);
+  setTimeout(func22, 6000);
+  setTimeout(func4, 8000);
+  setTimeout(func33, 8000);
+  setTimeout(func5, 10000);
+  setTimeout(func44, 10000);
+  setTimeout(func6, 12000);
+  setTimeout(func55, 12000);
+  setTimeout(func66, 14000);
+  setTimeout(func77, 16000);
+
+}
+
+function func1() {
+  $(".leter1").fadeTo(2000, 1);
+}
+function func2() {
+  $(".leter2").fadeTo(2000, 1);
+}
+function func3() {
+  $(".leter3").fadeTo(2000, 1);
+}
+function func4() {
+  $(".leter4").fadeTo(2000, 1);
+}
+function func5() {
+  $(".leter5").fadeTo(2000, 1);
+}
+function func6() {
+  $(".leter6").fadeTo(2000, 1);
+}
+function func11() {
+  $(".leter1").fadeOut(2000, 0);
+}
+function func22() {
+  $(".leter2").fadeOut(2000, 0);
+}
+function func33() {
+  $(".leter3").fadeOut(2000, 0);
+}
+function func44() {
+  $(".leter4").fadeOut(2000, 0);
+}
+function func55() {
+  $(".leter5").fadeOut(2000, 0);
+}
+function func66() {
+  $(".leter6").fadeOut(2000, 0)
+}
+
+function func77() {
+  $("#love").fadeTo(2000, 1);
+}
+
+// function myfn(){
+//   let str = '10000010010100010101101000100001010000110000100010011101011001000001000011001010000111000100001100011000011000010001000111101100100000100010010011000011111010000010001000010100001100001000011101010000010000110100100001111101000011001000001001010001010110100010000101000011000010001001110101100100000100001100101000011100010000110001100001100001000100011110110010000010001001001100001111101000001000100001010000110000100001110101000001000011010010000111110100001100101000011001110000111110101001101010'
+//   let sStr = ''
+//   let a = '<span>'
+//   let b = '</span>'
+//   let history = ''
+//   for(let i = 0; i<str.length;i++){
+//     sStr = a + str[i] + b;
+//     history += sStr
+//   }
+//   console.log(history)
+// }
+// myfn();
